de9f6bafb27e4c78f389e92816e01adbde0d92fb
Docker <docker@example.com>
2020-12-05 13:44:15 +0000

    rešen 4i dan

--- day_4/day_4.ml
+++ day_4/day_4.ml
@@ -41,11 +41,11 @@ let v_pare geslo =  (*geslo je zapisano kot "byr:1937\neyr:2030 pid:154364481\nh
 let veljaven_hgt podatek = 
     let konec = Str.last_chars podatek 2 in
     match konec with 
-    |"cm" -> let st = int_of_string (Str.first_chars podatek 3) in 
+    |"cm" -> let st = int_of_string (List.hd (Str.split (Str.regexp "cm") podatek)) in 
         if st >= 150 && st <= 193 then true else false
-    |"in" -> let st = int_of_string (Str.first_chars podatek 2) in 
+    |"in" -> let st = int_of_string (List.hd (Str.split (Str.regexp "in") podatek)) in 
         if st >= 59 && st <= 76 then true else false
-    |_-> failwith "neveljaven vnos"
+    |_-> false
 
 let rec veljaven_id_2 = function
     |[] -> true
@@ -71,20 +71,24 @@ let rec veljaven_id_2 = function
         |"pid" -> let regpid = Str.regexp "^[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]$" in
             if (Str.string_match regpid podatek 0) then veljaven_id_2 xs else false
         
-        |_-> veljaven_id_2 xs
+        |"cid" -> veljaven_id_2 xs
+
+        |_ -> false
 
 let stevec_veljavnih_id2 seznam = 
     let rec aux acc = function
         |[]-> acc
         |x::xs -> let vnos = v_pare x in
-            if veljaven_id_2 vnos then aux (acc+1) xs else aux acc xs
+            if veljaven_id x then 
+                if veljaven_id_2 vnos then aux (acc+1) xs else aux acc xs
+            else aux acc xs
     in
     aux 0 seznam
 
 let naloga2 vsebina = 
     let r = Str.regexp "\n\n" in
     let input_list = vsebina |> Str.split r in
-    string_of_int (stevec_veljavnih_id input_list)
+    string_of_int (stevec_veljavnih_id2 input_list)
 
 let _ =
     let preberi_datoteko ime_datoteke =
@@ -97,7 +101,9 @@ let _ =
         output_string chan vsebina;
         close_out chan
     in
-    let vsebina_datoteke = preberi_datoteko "day_3/day_3.in" in
-    let odgovor1 = naloga1 vsebina_datoteke
+    let vsebina_datoteke = preberi_datoteko "day_4/day_4.in" in
+    let odgovor1 = naloga1 vsebina_datoteke in
+    let odgovor2 = naloga2 vsebina_datoteke
     in
     izpisi_datoteko "day_4/day_4_1.out" odgovor1;
+    izpisi_datoteko "day_4/day_4_2.out" odgovor2
